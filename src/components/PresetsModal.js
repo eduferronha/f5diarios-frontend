73f69a0a4c5a0a7233a3cdc483e127fb4decdecb
Eduardo Ferronha <eduardoferronha@gmail.com>
2025-11-12 11:06:23 +0000

    esc and enter x7

--- src/components/PresetsModal.js
+++ src/components/PresetsModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import api from "../services/api";
 import TaskModal from "./TaskModel";
 import "../components/PresetsModel.css";
