096fed2b42e4f2c1173535cce4fa6be0f83fa823
Eduardo Ferronha <eduardoferronha@gmail.com>
2025-11-12 10:53:46 +0000

    esc and enter x4

--- src/components/TaskModel.js
+++ src/components/TaskModel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "../components/TaskModel.css";
