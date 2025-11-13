0b38ad5e3bb26f88cc53c31a45aff17089cda055
Eduardo Ferronha <eduardoferronha@gmail.com>
2025-11-13 17:07:42 +0000

    correções #22

--- src/pages/RelatoriosPage.js
+++ src/pages/RelatoriosPage.js
@@ -90,36 +90,16 @@ const RelatoriosPage = () => {
         const response = await api.get("/tasks/all", {
           headers: { Authorization: `Bearer ${token}` },
         });
-
-        // Espera até termos a lista de utilizadores carregada
-        const usersRes = await api.get("/users/", {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-
-        const users = usersRes.data;
-
-        // Junta username -> nome
-        const tarefasComNome = response.data.map((tarefa) => {
-          const user = users.find((u) => u.username === tarefa.username);
-          return {
-            ...tarefa,
-            username: user ? user.nome : tarefa.username, // substitui username pelo nome
-          };
-        });
-
-        setUtilizadores(users);
-        setDados(tarefasComNome);
-        setDadosOriginais(tarefasComNome);
+        setDados(response.data);
+        setDadosOriginais(response.data);
       } catch (error) {
         console.error("Erro ao carregar dados:", error);
       } finally {
         setLoading(false);
       }
     };
-
     carregarDados();
   }, [token]);
-
 
   // 🔹 Carregar listas
   useEffect(() => {
