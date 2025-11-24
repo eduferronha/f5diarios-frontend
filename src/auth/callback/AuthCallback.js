7ca0f76e3fb4d5bc77150de24ec27e4916f5d7a9
Eduardo Ferronha <eduardoferronha@gmail.com>
2025-11-24 14:57:51 +0000

    entra login x9

--- src/auth/callback/AuthCallback.js
+++ src/auth/callback/AuthCallback.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../services/api";
+import api from "../../services/api";
 
 function AuthCallback() {
   const navigate = useNavigate();
