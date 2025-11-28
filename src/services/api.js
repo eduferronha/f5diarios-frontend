c7a63ef683f862a25fb3edbcf2a350f7c44c9730
Eduardo Ferronha <eduardoferronha@gmail.com>
2025-11-28 12:17:46 +0000

    added vite url x2

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://127.0.0.1:8000", 
-  // baseURL: process.env.REACT_APP_API_URL,
-    baseURL: import.meta.env.VITE_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
+    // baseURL: import.meta.env.VITE_API_URL,
 
 });
 
